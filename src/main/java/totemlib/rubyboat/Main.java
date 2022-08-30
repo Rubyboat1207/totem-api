e003afe74c6234d730ec45cca42642873c21e64d
Rubyboat <rudy@rudysoliz.com>
2022-08-30 17:35:20 -0500

    1.19.2

--- src/main/java/totemlib/rubyboat/Main.java
+++ src/main/java/totemlib/rubyboat/Main.java
@@ -3,9 +3,6 @@ package totemlib.rubyboat;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
-    public static String MOD_ID = "totem_api";
-    //StatusEffects
-
     @Override
     public void onInitialize() {
     }
