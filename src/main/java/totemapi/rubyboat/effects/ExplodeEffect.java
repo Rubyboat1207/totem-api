8f790b91de54f0be853c3b3fca10bc9c5bbc113c
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-04-27 22:02:01 -0500

    Explode effect update

--- src/main/java/totemapi/rubyboat/effects/ExplodeEffect.java
+++ src/main/java/totemapi/rubyboat/effects/ExplodeEffect.java
@@ -14,7 +14,7 @@ public class ExplodeEffect extends DeezNuts{
 
     public void onDeath(LivingEntity user, World world, ItemStack stack) {
         user.setInvulnerable(true);
-        world.createExplosion(user, user.getX(), user.getY(), user.getZ(), power, World.ExplosionSourceType.MOB);
+        world.createExplosion(user, user.getX(), user.getY(), user.getZ(), power, World.ExplosionSourceType.TNT);
         user.setInvulnerable(false);
     }
 
